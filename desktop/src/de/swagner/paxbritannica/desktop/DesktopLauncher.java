67954bd09de5264b081009692a0e5720507cbcf4
Xerxes Rånby <xerxes@gudinna.com>
2015-08-21 22:33:29 +0200

    gdx-backend-jogamp: 1.6.5 require you to pick a Newt or Awt configuration.

--- desktop/src/de/swagner/paxbritannica/desktop/DesktopLauncher.java
+++ desktop/src/de/swagner/paxbritannica/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package de.swagner.paxbritannica.desktop;
 
-import com.badlogic.gdx.backends.jogamp.JoglApplication;
-import com.badlogic.gdx.backends.jogamp.JoglApplicationConfiguration;
+import com.badlogic.gdx.backends.jogamp.*;
 import de.swagner.paxbritannica.PaxBritannica;
 
 public class DesktopLauncher {
 	public static void main(String[] args) {
-		JoglApplicationConfiguration config = new JoglApplicationConfiguration();
+		JoglNewtApplicationConfiguration config = new JoglNewtApplicationConfiguration();
 		config.width = 1024;
 		config.height = 550;
 		config.title = "Pax Britannica";
-		new JoglApplication(new PaxBritannica(), config);
+		new JoglNewtApplication(new PaxBritannica(), config);
 	}
 }
